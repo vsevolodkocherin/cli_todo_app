3ae5403bd6744ad62a4dbb03d19b28745f8cdfc3
Vsevolod Kocherin <kocherin@Vsevolods-MBP.lan>
2023-01-09 11:29:12 +0200

    define unit tests for todoer.add()

--- todo/database.py
+++ todo/database.py
@@ -1,7 +1,9 @@
 import configparser
+import json
 from pathlib import Path
+from typing import Any, Dict, List, NamedTuple
 
-from todo import DB_WRITE_ERROR, SUCCESS
+from todo import DB_READ_ERROR, DB_WRITE_ERROR, JSON_ERROR, SUCCESS
 
 DEFAULT_DB_FILE_PATH = Path.home().joinpath(
     "." + Path.home().stem + "_todo.json"
@@ -20,3 +22,29 @@ def init_database(db_path: Path) -> int:
         return SUCCESS
     except OSError:
         return DB_WRITE_ERROR
+
+class DBResponse(NamedTuple):
+    todo_list: List[Dict[str, Any]]
+    error: int
+
+class DatabaseHandler:
+    def __init__(self, db_path: Path) -> None:
+        self._db_path = db_path
+
+    def read_todos(self) -> DBResponse:
+        try:
+            with self._db_path.open("r") as db:
+                try:
+                    return DBResponse(json.load(db), SUCCESS)
+                except json.JSONDecodeError:  # Catch wrong JSON format
+                    return DBResponse([], JSON_ERROR)
+        except OSError:  # Catch file IO problems
+            return DBResponse([], DB_READ_ERROR)
+
+    def write_todos(self, todo_list: List[Dict[str, Any]]) -> DBResponse:
+        try:
+            with self._db_path.open("w") as db:
+                json.dump(todo_list, db, indent=4)
+            return DBResponse(todo_list, SUCCESS)
+        except OSError:  # Catch file IO problems
+            return DBResponse(todo_list, DB_WRITE_ERROR)
---
+++ todo/todo.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from typing import Any, Dict, NamedTuple
+
+from todo.database import DatabaseHandler
+
+class CurrentTodo(NamedTuple):
+    todo: Dict[str, Any]
+    error: int
+
+class Todoer:
+    def __init__(self, db_path: Path) -> None:
+        self._db_handler = DatabaseHandler(db_path)
